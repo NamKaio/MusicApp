5c4d0e0d59babd6a9fae97ea7d0a6e84503eceae
NamKaio <43561584+NamKaio@users.noreply.github.com>
2020-07-02 11:09:15 +0700

    donevid24

--- app/src/main/java/com/example/musicapp/Fragment/FragmentBaiHatHot.java
+++ app/src/main/java/com/example/musicapp/Fragment/FragmentBaiHatHot.java
@@ -8,7 +8,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.musicapp.Adapter.BaihathotAdapter;
 import com.example.musicapp.Model.BaiHat;
 import com.example.musicapp.R;
 import com.example.musicapp.Services.APIService;
@@ -24,10 +27,13 @@ import retrofit2.Response;
 public class FragmentBaiHatHot extends Fragment {
 
     View view;
+    RecyclerView recyclerViewbaihathot;
+    BaihathotAdapter baihathotAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_bai_hat_hot, container, false);
+        recyclerViewbaihathot = view.findViewById(R.id.recycleviewbaihathot);
         GetData();
         return view;
     }
@@ -39,6 +45,11 @@ public class FragmentBaiHatHot extends Fragment {
             @Override
             public void onResponse(Call<List<BaiHat>> call, Response<List<BaiHat>> response) {
                 ArrayList<BaiHat> baiHatArrayList = (ArrayList<BaiHat>) response.body();
+                baihathotAdapter = new BaihathotAdapter(getActivity(), baiHatArrayList);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                recyclerViewbaihathot.setLayoutManager(linearLayoutManager);
+                recyclerViewbaihathot.setAdapter(baihathotAdapter);
             }
 
             @Override
